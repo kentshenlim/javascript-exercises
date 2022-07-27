dde0fea0e385ebb9f6759154341f64a6d3e7aa99
LessSmartKent <kentshen_lim24@hotmail.com>
2022-07-27 15:24:48 +0100

    Complete 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const len = str.length;
+    let final = '';
+    for (let i = len-1; i >= 0; i--) {
+        final += str[i];
+    }
+    return final;
 };
 
 // Do not edit below this line
