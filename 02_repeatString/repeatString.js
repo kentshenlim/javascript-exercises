c8a9857b939b1d6871868a41b8bfc5676d74fc4b
LessSmartKent <kentshen_lim24@hotmail.com>
2022-07-27 15:18:51 +0100

    Complete 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let final = '';
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        for (let i = 1; i <= num; i++) {
+            final += string;
+        }
+        return final;
+    }
 };
 
 // Do not edit below this line
