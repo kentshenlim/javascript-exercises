9e44065a756332e4c7db7ae98ac5d79308f23dd4
LessSmartKent <kentshen_lim24@hotmail.com>
2022-07-28 11:27:08 +0100

    Complete 07_rempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let ans = ((f-32)*5/9);
+  (!Number.isInteger(ans)) ? ans = ans.toFixed(1) : ans = ans;
+  return +ans;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let ans = (c*9/5+32);
+  (!Number.isInteger(ans)) ? ans = ans.toFixed(1) : ans = ans;
+  return +ans;
 };
 
 // Do not edit below this line
