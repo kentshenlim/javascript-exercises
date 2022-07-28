191f5aebc477fb7cc4373921845d45514ab25eca
LessSmartKent <kentshen_lim24@hotmail.com>
2022-07-28 11:08:23 +0100

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(lowerNum, upperNum) {
+    // Typeof check
+    if (lowerNum<0 || upperNum<0 || typeof lowerNum !== 'number' ||
+    typeof upperNum !== 'number') {
+        return 'ERROR';
+    }
 
+    // Swap if first arg bigger than second
+    if (lowerNum > upperNum) {
+        let temp = lowerNum;
+        lowerNum = upperNum;
+        upperNum = temp;
+    }
+
+    // Execute if two checks passed
+    let sum = 0;
+    for (let i = lowerNum; i <= upperNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
