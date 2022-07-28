78934cda1c84332aa46d2d9239490d3d40329b88
LessSmartKent <kentshen_lim24@hotmail.com>
2022-07-28 10:51:42 +0100

    Complete 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...optParam) {
+    for (item of optParam) {
+        array = removeOneFromArray(array, item);
+    }
+    return array;
 };
+
+function removeOneFromArray(array, one) {
+    return array.filter((item) => (item !== one));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
