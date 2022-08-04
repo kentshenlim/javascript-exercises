169b9fe3663c8407e96f02d5d672bfa56cc27e75
LessSmartKent <kentshen_lim24@hotmail.com>
2022-08-04 18:20:57 +0100

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+/*Check is letter or not, then form an array of char, then reverse then cmp*/
+const palindromes = function (str) {
+    let strArray = str.split('');
+    strArray = strArray.filter(item => {
+        return item.toUpperCase() !== item.toLowerCase()
+    });
+    return strArray.join('').toLowerCase() === 
+    strArray.reverse().join('').toLowerCase();
 };
 
 // Do not edit below this line
