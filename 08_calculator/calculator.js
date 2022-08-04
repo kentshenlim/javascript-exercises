d22779428aa3b3d46cc6715cf8ee7dcbbd77c15f
LessSmartKent <kentshen_lim24@hotmail.com>
2022-08-04 17:17:17 +0100

    Complete 09_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((runningSum, next) => {
+    return runningSum + next;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((cumProd, item) => {
+    return cumProd*item;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n == 1 || n == 0) return 1;
+  else if (n < 1) return undefined;
+  else return n*factorial(n-1);
 };
 
 // Do not edit below this line
