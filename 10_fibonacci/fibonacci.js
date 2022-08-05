e57bf270a2b35a258e98c8a416da2eb36f40b0af
LessSmartKent <kentshen_lim24@hotmail.com>
2022-08-05 10:21:45 +0100

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) return 'OOPS';
+    else if (n == 1 || n == 2) return 1;
+    else return fibonacci(n-1) + fibonacci(n-2);
 };
 
+// Without using recursive function
+const fibonacci2 = function(n) {
+    if (n < 0) return 'OOPS';
+    let array = [1,1];
+    while (array.length < n) {
+        array.push(array[array.length-1] + array[array.length-2]);
+    }
+    return array[n-1];
+}
+
 // Do not edit below this line
-module.exports = fibonacci;
+module.exports = fibonacci2;
