5bc0e783fbfc2414199e85321a79baf9af3ddbce
LessSmartKent <kentshen_lim24@hotmail.com>
2022-08-05 10:59:11 +0100

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arrayOfObj) {
+    const sortedArray = arrayOfObj.sort((a,b) => {
+        if (!('yearOfDeath' in a)) a.yearOfDeath = new Date().getFullYear();
+        if (!('yearOfDeath' in b)) b.yearOfDeath = new Date().getFullYear();
+        const aYear = a.yearOfDeath - a.yearOfBirth;
+        const bYear = b.yearOfDeath - b.yearOfBirth;
+        return bYear - aYear; // Descending
+    });
+    return sortedArray[0];
 };
 
 // Do not edit below this line
